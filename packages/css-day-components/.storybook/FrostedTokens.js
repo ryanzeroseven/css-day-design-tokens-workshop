771b8d13f2c03352b3ab5f321d82f1113956a5b4
Ian Frost <inf5000@gmail.com>
2025-06-04 07:18:32 +0200

    Remove URL from Storybook logo

--- packages/css-day-components/.storybook/FrostedTokens.js
+++ packages/css-day-components/.storybook/FrostedTokens.js
@@ -3,6 +3,5 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   brandTitle: `<img src="/assets/Logo.svg" alt="Frosted Tokens" style=max-width="250px" />`,
-  brandUrl: 'https://storybook.designtokenscourse.com/',
   brandTarget: '_self'
 });
